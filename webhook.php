e582e887d65e0497930634380fdaef99e303e319
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-06-26 13:51:36 +0200

    html to php

--- webhook.php
+++ webhook.php
@@ -1,9 +1,26 @@
 <?php
-if (isset($_POST['submit'])) {
 
-    // Variable initialisation 
-    $username = $_POST['username'];
-    $message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (empty($_POST["webhook"])) {
+        return;
+    } else {
+        $webhookurl = $_POST["webhook"];
+    }
+
+    if (empty($_POST["username"])) {
+        return;
+    } else {
+        $username = $_POST["username"];
+    }
+
+    if (empty($_POST["message"])) {
+        return;
+    } else {
+        $message = $_POST["message"];
+    }
+
+    // Text-To-Speech is disabled by default
     $tts = false;
 
     // Enable TTS if checked
@@ -14,12 +31,6 @@ if (isset($_POST['submit'])) {
     // https://gist.github.com/Mo45/cb0813cb8a6ebcd6524f6a36d4f8862c by Mo45
 
     //=======================================================================================================
-    // Create new webhook in your Discord channel settings and copy&paste URL
-    //=======================================================================================================
-
-    $webhookurl = "https://discord.com/api/webhooks/857900309317943318/E51kPhmH5g-_TJu5PaoM0p1PG4L2yRU2fmMPp8OC5u1qJVxa86Yl1mG3UMf8SyzsIWJ8";
-
-    //=======================================================================================================
     // Compose message. You can use Markdown
     // Message Formatting -- https://discordapp.com/developers/docs/reference#message-formatting
     //========================================================================================================
@@ -27,6 +38,7 @@ if (isset($_POST['submit'])) {
     $timestamp = date("c", strtotime("now"));
 
     $json_data = json_encode([
+        
         // Message
         "content" => $message,
 
@@ -45,7 +57,6 @@ if (isset($_POST['submit'])) {
 
     ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
 
-
     $ch = curl_init( $webhookurl );
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
     curl_setopt( $ch, CURLOPT_POST, 1);
@@ -53,13 +64,15 @@ if (isset($_POST['submit'])) {
     curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt( $ch, CURLOPT_HEADER, 0);
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
     $response = curl_exec( $ch );
+
     // If you need to debug, or find out why you can't send message uncomment line below, and execute script.
     // echo $response;
     curl_close( $ch );
 
     // Print success message
     print("Message has been sent!");
+        
+    unset($_POST);
 }
 ?>
